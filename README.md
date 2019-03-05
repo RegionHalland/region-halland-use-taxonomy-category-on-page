4fe5466fc97448e1c50aa9b1f9903aa291dfb12f
rhn014 <Roland.Hyden@RegionHalland.se>
2019-03-05 10:28:31 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# region-halland-use-taxonomy-category-on-page
-Plugin som gör det möjligt att använda taxonomy category även på sidor
+# Lägg till så att taxonomy category även kan användas på sidor
+
+## Hur man använder Region Hallands plugin "region-halland-use-taxonomy-category-on-page"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-use-taxonomy-category-on-page".
+
+
+## Användningsområde
+
+Denna plugin visar alla kategorier på en sida
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-use-taxonomy-category-on-page.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-use-taxonomy-category-on-page.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-use-taxonomy-category-on-page": "1.0.0"
+},
+```
+
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region_halland_use_taxonomy_category_on_page.php
@@ -0,0 +1,58 @@
+<?php
+
+	/**
+	 * @package Region Halland Use Taxonomy Category on Page
+	 */
+	/*
+	Plugin Name: Region Halland Use Taxonomy Category on Page
+	Description: Lägg till så att taxonomy category även kan användas på sidor
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: MIT
+	Text Domain: regionhalland
+	*/
+
+	// Registrera taxonomy category
+	add_action('init','add_region_halland_taxonomy_category_to_pages');
+	
+	// Registrera taxonomy category
+	function add_region_halland_taxonomy_category_to_pages() {
+      	register_taxonomy_for_object_type('category','page');
+  	} 
+	
+	// Sätt upp data
+	add_action('pre_get_posts','region_halland_taxonomy_category_archive');
+	
+	// Sätt upp data
+	function region_halland_taxonomy_category_archive($wp_query) {
+
+		// Kontrollera om det är en kategori
+		if($wp_query->is_main_query() && ! is_admin() && ($wp_query->is_category())){
+		    
+		    // Hämta array
+		    $my_post_array = array('post','page');
+		    
+		    // Sätt data
+		    if ($wp_query->get('category_name') || $wp_query->get('cat' )) {
+		     	$wp_query->set( 'post_type', $my_post_array );
+		    }
+		}
+	}
+
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_use_taxonomy_category_on_page_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen avaktiveras
+	function region_halland_use_taxonomy_category_on_page_deactivate() {
+		// Ingenting just nu...
+	}
+	
+	// Vilken metod som ska anropas när pluginen aktiveras
+	register_activation_hook( __FILE__, 'region_halland_use_taxonomy_category_on_page_activate');
+
+	// Vilken metod som ska anropas när pluginen avaktiveras
+	register_deactivation_hook( __FILE__, 'region_halland_use_taxonomy_category_on_page_deactivate');
+
+?>
